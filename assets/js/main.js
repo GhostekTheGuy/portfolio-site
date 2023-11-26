cc9c513c1b488da129200245b77ae2c584a5837f
Hubert Kolejko <ghostek.kontakt@gmail.com>
2023-11-26 22:45:32 +0100

    k

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,18 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+﻿
+
+const sr = ScrollReveal({ 
+  origin: 'top', 
+  distance: '60px', 
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal('.profile__border', {delay: 400});
+sr.reveal('.profile__name', {delay: 500});
+sr.reveal('.profile__profession', {delay: 600});
+sr.reveal('.profile__social', {delay: 700});
+sr.reveal('.profile__info-group', {interval: 100, delay: 700});
+sr.reveal('.profile__buttons', {delay: 800});
